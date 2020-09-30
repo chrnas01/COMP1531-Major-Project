974b91ee93b5fa34ff509a44ab3f92b00d9b1198
Christopher Nassif <z5308678@ad.unsw.edu.au>
2020-09-30 22:21:03 +1000

    Added tests for functions channels_list and channels_listall

--- src/channels_test.py
+++ src/channels_test.py
@@ -4,15 +4,29 @@ import channel
 import channels 
 from error import InputError         
 
-
+    ### Functions to be seperated out and file to be formatted and styled correctly ###
+    ## Include auth.delete_users() to ensure more accurate testing. 
 def test_channels_list(): 
 
-# Assume channels_create function is working 
-# Assume user is registered and logged in:  [For Tests 1 - 4]
+    # Assume user is registered and logged in:  [For Tests 1 - 4]
     auth.auth_register("chris@gmai.com, "password", "Chris", "Nassif")
     auth.auth_login("chris@gmail.com, "password")
 
+    # Test 1: 
+    channels.channels_create("chris@gmail.com", "Channel_1", True) == {'channel_id': 1,}
+    channel.channel_join()
 
+
+
+    # Test 1: Unsuccessful for unregistered user 
+    with pytest raises(InputError) as e: 
+        assert channels.channels_list("invaliduser@gmail.com") 
+
+    # Test 2: Registered users that dont belong to a channel should returh an empty dictionary 
+    auth.auth_register("JohnSmith@gmai.com, "password", "John", "Smith")
+    assert channels.channels_list("JohnSmith@gmail.com") == {}
+
+    # Test 3:
 
 def test_channels_listall(): 
 
@@ -20,15 +34,19 @@ def test_channels_listall():
     auth.auth_register("chris@gmai.com", "password", "Chris", "Nassif")
     auth.auth_login("chris@gmail.com", "password")
 
-    # Assume there are 3 existing channels 
-    channels.channels_create("chris@gmail.com", "Channel_1", True) == {channel id }
-    channels.channels_create("chris@gmail.com", "Channel_2", True)
-    channels.channels_create("chris@gmail.com", "Channel_3", False)
+    # Assume there is an existing private and public channel. 
+    channels.channels_create("chris@gmail.com", "Channel_1", True) == {'channel_id': 1,}
+    channels.channels_create("chris@gmail.com", "Channel_2", False) == {'channel_id': 2,}
     
-    channels.channels_listall("chris@gmai.com")
+    # Test 1:  Successful for registered and logged in user 
+    assert channels.channels_listall("chris@gmail.com") == 'channels': [{'channel_id': 1,'name': 'Channel_1',}, 
+    {'channel_id': 2,'name': 'Channel_2',}]
 
-
-
+    # Test 2: Unsuccessful for unregistered user 
+    with pytest.raises(InputError) as e: 
+        assert channels.channels_listall("unregistered@gmail.com")
+    
+   
 def test_channels_create(): 
 
 
@@ -40,6 +58,7 @@ def test_channels_create():
     # Test 1: Public channel name is greater than 20 characters long: 
     with pytest.raises(InputError) as e:
         assert channels.channels_create("chris@gmail.com", "ChannelNameGreaterthan20characters", True) 
+    
     # Test 2: Private channel name is greather than 20 characterss long: 
     with pytest.raises(InputError) as e: 
         assert channels.channels_create("chris@gmai.com", "ChannelNameGreaterthan20characters", False) 
@@ -51,7 +70,16 @@ def test_channels_create():
     assert channels.channels_create(chris@gmail.com, "ChannelName", False) == {'channel_id': 1}
 
     # Test 5: Unregistered/ Invalid user attempts to create channel 
+    ######## For Public 
+                ### Use auth.delete_users() function for better testing ###
+    
     with pytest.raises(InputError) as e: 
         assert channels.channels_create("unregistered@gmail.com", "ChannelName", True)
+    ######## For Private
+    with pytest.raises(InputError) as e: 
+        assert channels.channels_create("unregistered@gmail.com", "ChannelName", False)
+
+    # Test 6: Channel name already exists 
+    
     with pytest.raises(InputError) as e: 
         assert channels.channels_create("unregistered@gmail.com", "ChannelName", False)
