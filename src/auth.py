eb8ef9afbe9bf2fe0ae9b1973d7f1053426c0fae
Samuel He <samuelhe@xiaos-imac.lan>
2020-10-11 13:32:33 +1030

    Fixed pylint for auth.py and auth_test.py

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,16 @@
+'''
+functions for authentication of user
+'''
 import re
 from error import InputError
 import other
 
 
-# input email and password
 def auth_login(email, password):
+    '''
+    Given a registered users' email and password and generates a valid
+    token for the user to remain authenticated
+    '''
     # is email format valid
     regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$'
     if not re.search(regex, email):
@@ -16,7 +22,7 @@ def auth_login(email, password):
         raise InputError('Email is not registered - Cannot login')
 
     # password is correct
-    elif not is_password_correct(u_id, password):
+    if not is_password_correct(u_id, password):
         raise InputError('Password is incorrect - Cannot login')
 
     # calculate token
@@ -29,34 +35,45 @@ def auth_login(email, password):
 
 # provided a token, determines if logout is_success
 def auth_logout(token):
+    '''
+    Given an active token, invalidates the token to log the user out.
+    If a valid token is given, and the user is successfully logged out,
+    it returns true, otherwise false.
+    '''
     for user in other.data['users']:
         if user['token'] == token:
             return {'is_success ': True}
     return {'is_success ': False}
 
-# provided email, password, name_first, name_last
-# validate no input error was made
-# return u_id and token
 def auth_register(email, password, name_first, name_last):
+    '''
+    Given a user's first and last name, email address, and password,
+    create a new account for them and return a new token for
+    authentication in their session. A handle is generated that is the
+    concatentation of a lowercase-only first name and last name. If the
+    concatenation is longer than 20 characters, it is cutoff at 20 characters.
+    If the handle is already taken, you may modify the handle in any way you
+    see fit to make it unique.
+    '''
     # is email format valid
     regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$'
     if not re.search(regex, email):
         raise InputError('Email not of valid format - Cannot register')
 
     # password valid length
-    elif len(password) < 6:
+    if len(password) < 6:
         raise InputError('length of password is invalid - Cannot register')
 
     # name_first valid length
-    elif len(name_first) < 1 or len(name_first) > 50:
+    if len(name_first) < 1 or len(name_first) > 50:
         raise InputError('length of first name is invalid - Cannot register')
-    
+
     # name_last valid length
-    elif len(name_last) < 1 or len(name_last) > 50:
+    if len(name_last) < 1 or len(name_last) > 50:
         raise InputError('length of last name is invalid - Cannot register')
 
     # check if already registered email
-    elif is_email_registered(email):
+    if is_email_registered(email):
         raise InputError('Email is already registered - Cannot register')
 
     # register
@@ -75,36 +92,40 @@ def auth_register(email, password, name_first, name_last):
             #check if handle exists
             if handle_str == handle:
                 handle_exists = True
-    
+
     if handle_exists:
         handle_str = handle_str + str(u_id)
 
     new_register = {
-        'u_id': u_id, 
-        'token': token, 
-        'email': email, 
-        'password': password, 
-        'name_first': name_first, 
-        'name_last': name_last, 
+        'u_id': u_id,
+        'token': token,
+        'email': email,
+        'password': password,
+        'name_first': name_first,
+        'name_last': name_last,
         'handle_str': handle_str,
     }
 
     other.data['users'].append(new_register)
-    
+
     return {
         'u_id': u_id,
         'token': token,
     }
 
-# given an email, check if it is already registered
 def is_email_registered(email):
+    '''
+    given an email, check if it is already registered
+    '''
     for user in other.data['users']:
         if user['email'] == email:
             return user['u_id']
     return False
 
-# given u_id and password, check if they match the user
 def is_password_correct(u_id, password):
+    '''
+    given u_id and password, check if they match the user
+    '''
     # index 0 holds u_id 1
     if other.data['users'][u_id - 1]['password'] == password:
         return True
