eeeb0a0405ad5965acab94e584e90b99c85a4f0f
jayden <z5312070@ad.unsw.edu.au>
2020-10-11 16:36:59 +1100

    completed coding functions in user_test.py and user.py

--- src/user.py
+++ src/user.py
@@ -1,22 +1,104 @@
+import other
+import re
+import auth
+from error import InputError, AccessError
+
 def user_profile(token, u_id):
-    return {
-        'user': {
-        	'u_id': 1,
-        	'email': 'cs1531@cse.unsw.edu.au',
-        	'name_first': 'Hayden',
-        	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
-        },
-    }
+    # Check that the token is valid
+    if other.token_to_uid(token) <= 0:
+        raise AccessError("user_profile bad token")
+    
+    # seach for uid
+    for user in other.data['users']:
+        if user['u_id'] == u_id:
+            temp_dict = {
+                'email': user['email'],
+                'u_id': user['u_id'],
+                'name_first': user['name_first'],
+                'name_last': user['name_last'],
+                'handle_str': user['handle_str'],
+            }
+            return {'user': temp_dict}
+    else: 
+        raise InputError('u_id does not refer to a valid user')
+
+
 
 def user_profile_setname(token, name_first, name_last):
+    current_user = other.token_to_uid(token)
+    # Check that the token is valid
+    if current_user <= 0:
+        raise AccessError("user_profile_setname bad token")
+
+    # name_first valid length
+    if len(name_first) < 1 or len(name_first) > 50:
+        raise InputError('length of first name is invalid - Cannot register')
+    
+    # name_last valid length
+    if len(name_last) < 1 or len(name_last) > 50:
+        raise InputError('length of last name is invalid - Cannot register')
+
+    for user in other.data['users']:
+        if user['u_id'] == current_user:
+            user['name_first'] = name_first
+            user['name_last'] = name_last
+            break
+    else: 
+        raise InputError('u_id does not refer to a valid user')            
+
     return {
     }
 
 def user_profile_setemail(token, email):
+    current_user = other.token_to_uid(token)
+    # Check that the token is valid
+    if current_user <= 0:
+        raise AccessError("user_profile_setemail bad token")
+
+    # is email format valid
+    regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$'
+    if not re.search(regex, email):
+        raise InputError('Email not of valid format - Cannot register')
+
+    # check if already registered email
+    elif auth.is_email_registered(email):
+        raise InputError('Email is already registered - Cannot register')
+
+    for user in other.data['users']:
+        if user['u_id'] == current_user:
+            user['email'] = email
+            break
+    else: 
+        raise InputError('u_id does not refer to a valid user')            
+
     return {
     }
 
 def user_profile_sethandle(token, handle_str):
+    current_user = other.token_to_uid(token)
+    # Check that the token is valid
+    if current_user <= 0:
+        raise AccessError("user_profile_sethandle bad token")
+
+    handle_exists = False
+
+    # Make handle unique
+    if other.data['users']:
+        for user in other.data['users']:
+            handle = user.get('handle_str')
+            #check if handle exists
+            if handle_str == handle:
+                handle_exists = True
+    
+    if handle_exists:
+        handle_str = handle_str + str(current_user)
+
+    for user in other.data['users']:
+        if user['u_id'] == current_user:
+            user['handle_str'] = handle_str
+            break
+    else: 
+        raise InputError('u_id does not refer to a valid user')            
+
     return {
     }
---
+++ src/user_test.py
@@ -0,0 +1,161 @@
+import pytest
+import auth
+import channel
+import channels
+import user
+from error import InputError, AccessError
+import other
+
+########################################################
+
+def test_user_profile_success():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    assert user.user_profile(user_1['token'], user_1['u_id']) == {
+        'user': {
+                'email': 'jayden@gmail.com',
+                'u_id': 1,
+                'name_first': 'Jayden',
+                'name_last': 'Leung',
+                'handle_str': 'jaydenleung',
+            }
+        }
+
+def test_user_profile_invalid_uid():
+    other.clear()
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        user.user_profile(user_1['token'], 99)
+
+########################################################
+
+def test_user_profile_setname_success():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    user.user_profile_setname(user_1['token'], 'changed_first', 'changed_last')
+
+    assert user.user_profile(user_1['token'], user_1['u_id']) == {
+        'user': {
+                'email': 'jayden@gmail.com',
+                'u_id': 1,
+                'name_first': 'changed_first',
+                'name_last': 'changed_last',
+                'handle_str': 'jaydenleung',
+            }
+        }
+
+def test_user_profile_firstname_short():
+    other.clear()
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user_1['token'], '', 'LASTNAME') # name_first is length 0
+    
+def test_user_profile_firstname_long():
+    other.clear()
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user_1['token'], 'A'*51, 'LASTNAME') # name_first is length 51
+
+def test_user_profile_lastname_short():
+    other.clear()
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user_1['token'], 'FIRSTNAME', '') # name_last is length 0
+    
+def test_user_profile_lastname_long():
+    other.clear()
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user_1['token'], 'FIRSTNAME', 'A'*51) # name_last is length 51
+  
+
+########################################################
+
+def test_user_profile_setemail_success():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+    user.user_profile_setemail(user_1['token'], 'test@gmail.com')
+
+    assert user.user_profile(user_1['token'], user_1['u_id']) == {
+        'user': {
+                'email': 'test@gmail.com',
+                'u_id': 1,
+                'name_first': 'Jayden',
+                'name_last': 'Leung',
+                'handle_str': 'jaydenleung',
+            }
+        }
+
+def test_user_profile_setemail_invalid_email():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setemail(user_1['token'], 'EMAIL')
+    
+def test_user_profile_setemail_used_email():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+    user_2 = auth.auth_register('Steven@gmail.com', 'password', 'Steven', 'Luong')
+
+    with pytest.raises(InputError):
+        assert user.user_profile_setemail(user_1['token'], 'Steven@gmail.com')
+
+########################################################
+
+def test_user_profile_sethandle_success():
+    other.clear()
+
+    # Setup users
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+    user.user_profile_sethandle(user_1['token'], 'newhandle')
+
+    assert user.user_profile(user_1['token'], user_1['u_id']) == {
+        'user': {
+                'email': 'jayden@gmail.com',
+                'u_id': 1,
+                'name_first': 'Jayden',
+                'name_last': 'Leung',
+                'handle_str': 'newhandle',
+            }
+        }
+
+def test_user_profile_sethandle_already_exists():
+    other.clear()
+    user_1 = auth.auth_register('jayden@gmail.com', 'password', 'Jayden', 'Leung') # Flockr Owner
+    user_2 = auth.auth_register('Steven@gmail.com', 'password', 'Steven', 'Luong')
+    
+    user.user_profile_sethandle(user_1['token'], 'stevenluong1')
+
+    assert user.user_profile(user_1['token'], user_1['u_id']) == {
+        'user': {
+                'email': 'jayden@gmail.com',
+                'u_id': 1,
+                'name_first': 'Jayden',
+                'name_last': 'Leung',
+                'handle_str': 'stevenluong1',
+            }
+        }
+
+########################################################
