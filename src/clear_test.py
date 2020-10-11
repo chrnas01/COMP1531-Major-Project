7895df7acb3e3c5bad743eeea082dbda9d6bad2e
Samuel He <samuelhe@xiaos-imac.lan>
2020-10-11 15:34:59 +1030

    fixed pylint errors for all iteration 1 files

--- src/clear_test.py
+++ src/clear_test.py
@@ -1,30 +1,31 @@
-import pytest
-import error
+'''
+tests for the clear function
+'''
 import other
 import auth
 import channels
 
 def test_user_clear_1():
+    '''
+    test clearing a user
+    '''
     auth.auth_register('test0@email.com', 'password', 'name_first0', 'name_last0')
     other.clear()
-    assert bool(other.data['users']) == False
+    assert not bool(other.data['users'])
 
 def test_user_clear_0():
+    '''
+    test clearing zero users
+    '''
     other.clear()
-    assert bool(other.data['users']) == False
-
-def test_user_clear_10():
-    for counter in range(10):
-        email = 'test' + str(counter) + '@email.com'
-        firstname = 'name_first' + str(counter)
-        lastname = 'name_last' + str(counter)
-        auth.auth_register(email, 'password', firstname, lastname)
-    other.clear()
-    assert bool(other.data['users']) == False
+    assert not bool(other.data['users'])
 
 def test_channel_clear_1():
+    '''
+    test clearing a channel
+    '''
     auth.auth_register('test0@email.com', 'password', 'name_first0', 'name_last0')
     channels.channels_create(other.data['users'][0]['token'], 'test_channel', True)
     other.clear()
-    assert bool(other.data['users']) == False
-    assert bool(other.data['channels']) == False
+    assert not bool(other.data['users'])
+    assert not bool(other.data['channels'])
