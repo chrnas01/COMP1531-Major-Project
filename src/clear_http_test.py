1e613dda5f1e0e8c6166a53a644b88af44fd80df
Nick <z5311767@ad.unsw.edu.au>
2020-10-22 01:19:44 +1100

    Added clear functions

--- src/clear_http_test.py
+++ src/clear_http_test.py
@@ -4,28 +4,49 @@ tests for the http clear function
 import other
 import auth
 import channels
+import requests
+import json
+from echo_http_test import url
 
-def test_http_user_clear_1():
+def test_http_user_clear_1(url):
     '''
     test clearing a user
     '''
-    other.clear()
-    requests.post(url + 'auth/register', params={'email': 'nicholas@gmail.com', 'password': 'password', 'name_first': 'nicholas', 'name_last': 'tan'})
+    payload = {
+        'email': 'nicholas@gmail.com',
+        'password': 'password',
+        'name_first': 'nicholas',
+        'name_last': 'tan'
+    }
+    requests.post(url + 'auth/register', params=payload)
+    requests.delete(url + 'clear')
     assert other.is_empty()
 
-def test_http_user_clear_0():
+def test_http_user_clear_0(url):
     '''
     test clearing zero users
     '''
-    other.clear()
+    requests.delete(url + 'clear')
     assert other.is_empty()
 
-def test_http_channel_clear_1():
+def test_http_channel_clear_1(url):
     '''
     test clearing a channel
     '''
-    other.clear()
-    resp = requests.post(url + 'auth/register', params={'email': 'nicholas@gmail.com', 'password': 'password', 'name_first': 'nicholas', 'name_last': 'tan'})
-    requests.post(url + 'channels/create', params={'token': json.loads(resp.text)['token'], 'name': 'my_channel', 'is_public': True)    
-    assert other.is_empty()
+    payload = {
+        'email': 'nicholas@gmail.com',
+        'password': 'password',
+        'name_first': 'nicholas',
+        'name_last': 'tan'
+    }
+    resp = requests.post(url + 'auth/register', params=payload)
+
+    payload = {
+        'token': resp.json()['token'],
+        'name': 'my_channel',
+        'is_public': True
+    }
+    requests.post(url + 'channels/create', params=payload) 
     
+    requests.delete(url + 'clear')
+    assert other.is_empty()
