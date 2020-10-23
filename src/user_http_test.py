db0cc466cea90035f2c0f26dcf0b5f3b4708061b
Stven Luong <z5308641@ad.unsw.edu.au>
2020-10-23 15:24:54 +1100

    ok

---
+++ src/user_http_test.py
@@ -0,0 +1,10 @@
+import pytest
+import re
+from subprocess import Popen, PIPE
+import signal
+from time import sleep
+import requests
+import json
+from echo_http_test import url
+import auth
+import other
