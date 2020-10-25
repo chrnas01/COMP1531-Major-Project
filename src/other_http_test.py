54ccd27401db423b3f11abed24004e62d2e7ccac
Samuel He <samuelhe@xiaos-imac.lan>
2020-10-25 13:09:25 +1030

    added tests for search

--- src/other_http_test.py
+++ src/other_http_test.py
@@ -1,6 +1,7 @@
 '''
 Tests functions in other.py
 '''
+from datetime import datetime, timezone
 import json
 import requests
 import other
@@ -75,7 +76,7 @@ def test_admin_userpermission_change_invalid_permission_id(url, setup):
     payload = {
         'token': user_1.json()['token'],
         'u_id': user_2.json()['u_id'],
-        'permission_id': 2
+        'permission_id': 0
     }
     resp = requests.post(url + 'admin/userpermission/change', json=payload)
     assert "code" in resp.json()
@@ -123,4 +124,161 @@ def test_admin_userpermission_change_success(url, setup):
     resp = requests.post(url + 'other/successful/permissions', json=payload)
     assert json.loads(resp.text) == {'successful': True}
 
-###############################################################################
+################################################################################
+
+def test_search(url, setup):
+    '''
+    searching in a channel
+    '''
+    user_1, user_2, _ = setup
+
+    payload = {
+        'token': user_1.json()['token'],
+        'name': 'test channel',
+        'is_public': False
+    }
+    channel_data = requests.post(url + 'channels/create', json=payload).json()
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'u_id': user_2.json()['u_id']
+    }
+    requests.post(url + 'channel/invite', json=payload).json()
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'msg'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'test'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'Hello'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_2.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'test2'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    expected_result = {
+        'messages': [
+            {
+                'message_id': 2,
+                'channel_id': channel_data['channel_id'],
+                'u_id': user_1.json()['u_id'],
+                'message': 'test',
+                'time_created': int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
+            },
+            {
+                'message_id': 4,
+                'channel_id': channel_data['channel_id'],
+                'u_id': user_2.json()['u_id'],
+                'message': 'test2',
+                'time_created': int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
+            }
+        ]
+    }
+
+    payload = {
+        'token': user_1.json()['token'],
+        'query_str': 'est',
+    }
+
+    resp = requests.get(url + 'other/search', params=payload)
+    assert resp.json() == expected_result
+
+def test_search_other_channel(url, setup):
+    '''
+    searching in a separate channel
+    '''
+    user_1, user_2, _ = setup
+
+    payload = {
+        'token': user_1.json()['token'],
+        'name': 'test channel',
+        'is_public': False
+    }
+    channel_data = requests.post(url + 'channels/create', json=payload).json()
+
+    payload = {
+        'token': user_2.json()['token'],
+        'name': 'test channel2',
+        'is_public': False
+    }
+    channel_data2 = requests.post(url + 'channels/create', json=payload).json()
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'u_id': user_2.json()['u_id']
+    }
+    requests.post(url + 'channel/invite', json=payload).json()
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'msg'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'test'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_1.json()['token'],
+        'channel_id': channel_data['channel_id'],
+        'message': 'Hello'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    payload = {
+        'token': user_2.json()['token'],
+        'channel_id': channel_data2['channel_id'],
+        'message': 'test2'
+    }
+    requests.post(url + 'message/send', json=payload)
+
+    expected_result = {
+        'messages': [
+            {
+                'message_id': 2,
+                'channel_id': channel_data['channel_id'],
+                'u_id': user_1.json()['u_id'],
+                'message': 'test',
+                'time_created': int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
+            },
+            {
+                'message_id': 3,
+                'channel_id': channel_data['channel_id'],
+                'u_id': user_1.json()['u_id'],
+                'message': 'Hello',
+                'time_created': int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
+            }
+        ]
+    }
+
+    payload = {
+        'token': user_1.json()['token'],
+        'query_str': 'e',
+    }
+
+    resp = requests.get(url + 'other/search', params=payload)
+    assert resp.json() == expected_result
