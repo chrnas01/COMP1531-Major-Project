87c9cdcebaa8b45488e24139e110865773aa3ab0
Christopher Nassif <z5308678@ad.unsw.edu.au>
2020-11-08 18:00:18 +1100

    Formatted tests for standup.py and added necessary imports

---
+++ src/standup_test.py
@@ -0,0 +1,21 @@
+'''
+tests for standup.py 
+''' 
+
+import pytest
+import standup 
+from error import InputError, AccessError
+
+# Tests for standup_start() function.
+###################################################################################
+
+
+# Tests for standup_active() function.
+###################################################################################
+
+
+# Tests for standup_send() function.
+###################################################################################
+
+
+
