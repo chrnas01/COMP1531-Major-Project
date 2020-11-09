0390f5ef0d036604e753fad719aa3e038feefdc6
jayden <z5312070@ad.unsw.edu.au>
2020-11-10 01:28:42 +1100

    coded test_user_profile_uploadphoto_invalid_x_end_small

--- src/uploadphoto_test.py
+++ src/uploadphoto_test.py
@@ -164,3 +164,31 @@ def test_user_profile_uploadphoto_invalid_y_start_big(url):
 
     assert resp.status_code == 400
 
+
+def test_user_profile_uploadphoto_invalid_x_end_small(url):
+    '''
+    Tests a bad url for upload photo
+    '''
+    requests.delete(url + 'clear')
+
+    # Setup user
+    payload = {
+        'email': 'jayden@gmail.com',
+        'password': 'password',
+        'name_first': 'Jayden',
+        'name_last': 'Leung'
+    }
+    user_1 = requests.post(url + 'auth/register', json=payload).json()
+
+    payload = {
+        'token': user_1['token'],
+        'img_url': 'https://img1.looper.com/img/gallery/things-only-adults-notice-in-shrek/intro-1573597941.jpg',
+        'x_start': '0',
+        'y_start': '0',
+        'x_end': '-1',
+        'y_end': '20',
+    }
+    resp = requests.post(url + '/user/profile/uploadphoto', json=payload)
+
+    assert resp.status_code == 400
+
